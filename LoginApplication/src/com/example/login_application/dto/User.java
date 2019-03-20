c2a098187057968cde5d5fc410cc514650902eac
Vipul Sharma <vipuls526@gmail.com>
2019-03-20 16:40:43 +0530

    bean

---
+++ LoginApplication/src/com/example/login_application/dto/User.java
@@ -0,0 +1,24 @@
+/*
+ * DTO = Data Transfer Objects (Model Objects)
+ */
+package com.example.login_application.dto;
+
+public class User {
+	private String username;
+	private String name;
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
